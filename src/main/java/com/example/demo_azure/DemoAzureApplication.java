b8749c610adf29ad1bcd4c69b8625e376821b573
Nischal Gaire <nischalgaire@Nischals-Mac-mini.local>
2026-03-06 01:01:23 -0600

    Add restcontroller

--- src/main/java/com/example/demo_azure/DemoAzureApplication.java
+++ src/main/java/com/example/demo_azure/DemoAzureApplication.java
@@ -3,17 +3,23 @@ package com.example.demo_azure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoAzureApplication {
 
+	@GetMapping("/")
+	public String home() {
+		return "Your app deployed successfully!";
+	}
+
 	@GetMapping("/message")
-	public String message(){
-		return "Your app deployed successfully";
+	public String message() {
+		return "Your app deployed successfully!";
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAzureApplication.class, args);
 	}
-
 }
